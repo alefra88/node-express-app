afc1421fdce16566954586254a847564a74f305a
alefra88 <efra.rlz@gmail.com>
2023-06-02 04:41:45 -0600

    server

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const http = require("http");
+
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Home Page</h1>");
-  res.end();
+  const url = req.url;
+  //home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>something </h1>");
+    res.end();
+  }
+  //about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About </h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Page not found </h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
-console.log("Express Tutorial");
