c5ef91d95cb4a84c9da1f01f1f8c70f63a3ae062
alefra88 <efra.rlz@gmail.com>
2023-06-03 06:01:03 -0600

    add routes

--- app.js
+++ app.js
@@ -2,19 +2,42 @@ const http = require("http");
 const { readFileSync } = require("fs");
 
 //get all files
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
+
 const server = http.createServer((req, res) => {
   const url = req.url;
+  console.log(url);
   //home page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>something </h1>");
+    res.write(homePage);
     res.end();
   }
   //about page
   else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>About </h1>");
+    res.end();
+  }
+  //homestyles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  //homeImage
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  }
+  //HomeLogic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/js" });
+    res.write(homeLogic);
     res.end();
   }
   // 404
