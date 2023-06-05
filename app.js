6a7a09292bfb86967a28b43a94ecdd5399825607
alefra88 <efra.rlz@gmail.com>
2023-06-05 03:57:01 -0600

    add rustics endpoints

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const { products } = require("./data");
 
-// setup static and middlewares
+app.get("/resources", (req, res) => {
+  res.json(products);
+});
+
+//setup static and middlewares
 app.use(express.static("./public"));
 
+//Home Page
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
 
-// Home Page
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// });
-
-
-// 404
+//404
 app.all("*", (req, res) => {
   res.status(404).send("<h1>Page not found</h1>");
 });
