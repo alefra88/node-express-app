adc5ff45fb0be7ecb5d7db9cdbc10d28077b5e13
alefra88 <efra.rlz@gmail.com>
2023-06-06 07:34:53 -0600

    query stuffs

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.get("/api/v1/query", (req, res) => {
     sortedProducts = sortedProducts.slice(0, Number(limit));
   }
   if (sortedProducts.length < 1) {
-    res.status(200).send("no products matched your search");
+    // res.status(200).send("no products matched your search");
+    return res.status(200).json({ success: true, data: [] });
   }
   res.status(200).json(sortedProducts);
 });
