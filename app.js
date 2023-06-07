855ef93fe10e34de51975b9c5c77dbc7b8976d21
alefra88 <efra.rlz@gmail.com>
2023-06-07 05:42:52 -0600

    using morgan

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const logger = require("./logger.js");
 const authorize = require("./authorize.js");
+const morgan = require("morgan");
 //midleware
 // app.use([logger, authorize]);
+app.use(morgan("tiny"));
 app.get("/", (req, res) => {
   res.send("HOME HARDCODED");
 });
