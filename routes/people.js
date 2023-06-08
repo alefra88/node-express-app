5942344bb84a4702ba854e1fc620802081602a48
alefra88 <efra.rlz@gmail.com>
2023-06-08 04:43:37 -0600

    add

---
+++ routes/people.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+} = require('../controllers/people')
+
+// router.get('/', getPeople)
+// router.post('/', createPerson)
+// router.post('/postman', createPersonPostman)
+// router.put('/:id', updatePerson)
+// router.delete('/:id', deletePerson)
+
+router.route('/').get(getPeople).post(createPerson)
+router.route('/postman').post(createPersonPostman)
+router.route('/:id').put(updatePerson).delete(deletePerson)
+
+module.exports = router
+
+
+
+
+// app.get('/api/people', (req, res) => {
+//   res.status(200).json({ success: true, data: people })
+// })
+
+// app.post('/api/people', (req, res) => {
+//   const { name } = req.body
+//   if (!name) {
+//     return res
+//       .status(400)
+//       .json({ success: false, msg: 'please provide name value' })
+//   }
+//   res.status(201).json({ success: true, person: name })
+// })
+
+// app.post('/api/postman/people', (req, res) => {
+//   const { name } = req.body
+//   if (!name) {
+//     return res
+//       .status(400)
+//       .json({ success: false, msg: 'please provide name value' })
+//   }
+//   res.status(201).json({ success: true, data: [...people, name] })
+// })
+
+// app.post('/login', (req, res) => {
+//   const { name } = req.body
+//   if (name) {
+//     return res.status(200).send(`Welcome ${name}`)
+//   }
+
+//   res.status(401).send('Please Provide Credentials')
+// })
+
+// app.put('/api/people/:id', (req, res) => {
+//   const { id } = req.params
+//   const { name } = req.body
+
+//   const person = people.find((person) => person.id === Number(id))
+
+//   if (!person) {
+//     return res
+//       .status(404)
+//       .json({ success: false, msg: `no person with id ${id}` })
+//   }
+//   const newPeople = people.map((person) => {
+//     if (person.id === Number(id)) {
+//       person.name = name
+//     }
+//     return person
+//   })
+//   res.status(200).json({ success: true, data: newPeople })
+// })
+
+// app.delete('/api/people/:id', (req, res) => {
+//   const person = people.find((person) => person.id === Number(req.params.id))
+//   if (!person) {
+//     return res
+//       .status(404)
+//       .json({ success: false, msg: `no person with id ${req.params.id}` })
+//   }
+//   const newPeople = people.filter(
+//     (person) => person.id !== Number(req.params.id)
+//   )
+//   return res.status(200).json({ success: true, data: newPeople })
+// })
